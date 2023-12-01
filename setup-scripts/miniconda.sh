49e865cd2ce5945fd0ab3d88c2497df46ec9f9d3
Cameron Perot <cameronperot@users.noreply.github.com>
2023-12-02 00:35:39 +0100

    Added miniconda script

---
+++ setup-scripts/miniconda.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P )"
+
+# Setting up the miniconda directory
+INSTALL_DIR="${HOME}/miniconda31"
+CONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sudo rm -rf "${INSTALL_DIR}"
+sudo mkdir "${INSTALL_DIR}"
+sudo chown "${USER}:${USER}" "${INSTALL_DIR}"
+
+# Download and run the installer
+cd "${INSTALL_DIR}"
+sudo wget -O miniconda.sh "${CONDA_URL}"
+bash miniconda.sh -b -f -p "${INSTALL_DIR}"
+rm -f miniconda.sh
+
+# Update and install packages
+export PATH="${INSTALL_DIR}/bin:${PATH}"
+conda update -y conda
