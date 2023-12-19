9c8b59974011e34fa7f0689b707fa61da211c005
Cameron Perot <cameronperot@users.noreply.github.com>
2023-12-19 20:07:13 +0100

    Updated miniconda install script

--- setup-scripts/miniconda.sh
+++ setup-scripts/miniconda.sh
@@ -6,13 +6,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd -P )"
 # Setting up the miniconda directory - default to ~/miniconda
 INSTALL_DIR="${1:-${HOME}/miniconda}"
 CONDA_URL=https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sudo rm -rf "${INSTALL_DIR}"
-sudo mkdir "${INSTALL_DIR}"
-sudo chown "${USER}:${USER}" "${INSTALL_DIR}"
+rm -rf "${INSTALL_DIR}"
+mkdir "${INSTALL_DIR}"
 
 # Download and run the installer
 cd "${INSTALL_DIR}"
-sudo wget -O miniconda.sh "${CONDA_URL}"
+wget -O miniconda.sh "${CONDA_URL}"
 bash miniconda.sh -b -f -p "${INSTALL_DIR}"
 rm -f miniconda.sh
 
