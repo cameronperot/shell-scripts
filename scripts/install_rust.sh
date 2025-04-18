ccbd1b954790f7eb9ac9ff78b940a016636a613d
Cameron Perot <cameronperot@users.noreply.github.com>
2025-04-18 04:57:23 +0200

    Updated rust script

--- scripts/install_rust.sh
+++ scripts/install_rust.sh
@@ -5,7 +5,6 @@ echo "Starting rust installation..."
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "${HOME}/.cargo/env"
 cargo install \
-    mdbook \
     stylua \
     tree-sitter-cli
 echo "Rust installation completed successfully!"
